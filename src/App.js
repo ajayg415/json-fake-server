7b78a9ad926fa4fbe465b7cef0e6b8e38bf09772
ajayg415 <ajay.g415@gmail.com>
2020-08-15 00:27:56 +0530

    added json-server and concurrently module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    (async ()=>{
+      const call = await fetch(`http://localhost:5000/customers`);
+      const data = await call.json();
+      setData(data);
+    })();
+  },[])
+
+  console.table(data)
   return (
     <div className="App">
       <header className="App-header">
-
+        Open console to check ajax data
       </header>
     </div>
   );
